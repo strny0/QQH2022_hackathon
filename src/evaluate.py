9115808cfae49b103a0af0f6dc766752789e0ac2
Ondřej Hubáček <ondrej.hubacek@ematiq.com>
2022-11-26 09:34:59 +0100

    Updated evaluate script.

--- src/evaluate.py
+++ src/evaluate.py
@@ -9,7 +9,7 @@ from environment import Environment
 dataset = pd.read_csv('../data/training_data.csv', parse_dates=['Date', 'Open'])
 model = Model()
 env = Environment(dataset, model, init_bankroll=1000., min_bet=5., max_bet=100.)
-evaluation = env.run(start=pd.to_datetime('2005-08-01'))
+evaluation = env.run(start=pd.to_datetime('2014-08-01'))
 
 print(f'Final bankroll: {env.bankroll:.2f}')
 
